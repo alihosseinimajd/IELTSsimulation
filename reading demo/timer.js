5d486cd74014eebd1a0636b39b7205cf90800fb6
alihosseinimajd <68269259+alihosseinimajd@users.noreply.github.com>
2024-04-29 18:51:41 +0330

    Add files via upload

---
+++ reading demo/timer.js
@@ -0,0 +1,28 @@
+let timerInterval;
+let minuts = 0;
+let hours = 0;
+let seconds = 0;
+
+function startTimer() {
+    timerInterval = setInterval(updateTimer, 1000);
+}
+
+function updateTimer() {
+    seconds++;
+    if (seconds === 60) {
+     seconds = 0;  
+     minuts++; 
+    }
+
+    if (minuts === 60) {
+        minuts = 0;
+        hours++;
+    }
+    document.getElementById('timer').textContent = formatTime(hours,minuts,seconds)
+}
+function formatTime (hours, minuts, seconds) {
+    return `${padZero(hours)}:${padZero(minuts)}:${padZero(seconds)}`
+}
+function padZero(value){
+    return value.toString().padStart(2, '0');
+}
